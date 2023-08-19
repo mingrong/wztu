003b821888ee36c0e685151a00d59639119108fe
mingrong <xmr95123654@gmail.com>
2023-08-20 01:39:22 +1000

    Delete labs directory

--- labs/03_neural_recsys/solutions/similarity.py
+++
@@ -1,7 +0,0 @@
-EPSILON = 1e-07
-
-
-def cosine(x, y):
-    dot_products = np.dot(x, y.T)
-    norm_products = np.linalg.norm(x) * np.linalg.norm(y)
-    return dot_products / (norm_products + EPSILON)
